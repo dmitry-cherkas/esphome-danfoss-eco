77446765ee3448fe7153fa66f7ddcc7c49035bc0
Dmitry Cherkas <cherkas.da@gmail.com>
2021-11-22 01:08:07 +0300

    move helper functions to cpp file

---
+++ components/danfoss_eco/helpers.cpp
@@ -0,0 +1,42 @@
+#include "esphome/core/helpers.h"
+#include "esphome/core/log.h"
+#include "helpers.h"
+
+#define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
+namespace esphome
+{
+    namespace danfoss_eco
+    {
+        uint8_t *parse_hex_str(const char *value, size_t str_len)
+        {
+            size_t len = str_len / 2;
+            uint8_t *buff = (uint8_t *)malloc(sizeof(uint8_t) * len);
+            for (size_t i = 0; i < len; i++)
+                buff[i] = (parse_hex(value[i * 2]).value() << 4) | parse_hex(value[i * 2 + 1]).value();
+
+            return buff;
+        }
+
+        int parse_int(uint8_t *data, int start_pos)
+        {
+            return int(data[start_pos] << 24 | data[start_pos + 1] << 16 | data[start_pos + 2] << 8 | data[start_pos + 3]);
+        }
+
+        bool parse_bit(uint8_t value, int pos)
+        {
+            return (value & (1 << pos)) >> pos;
+        }
+
+        void reverse_chunks(uint8_t *data, int len, uint8_t *reversed_buf)
+        {
+            for (int i = 0; i < len; i += 4)
+            {
+                int l = MIN(4, len - i); // limit for a chunk, 4 or what's left
+                for (int j = 0; j < l; j++)
+                {
+                    reversed_buf[i + j] = data[i + (l - 1 - j)];
+                }
+            }
+        }
+    }
+}
--- components/danfoss_eco/helpers.h
+++ components/danfoss_eco/helpers.h
@@ -1,46 +1,16 @@
 #pragma once
 
-#include "esphome/core/helpers.h"
-#include "esphome/core/log.h"
-
-#define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
 namespace esphome
 {
     namespace danfoss_eco
     {
-        static const char *const TAG = "danfoss_eco";
-        static const uint16_t NAN_HANDLE = -1;
+        const char *const TAG = "danfoss_eco";
+        const uint16_t NAN_HANDLE = -1;
 
-        static uint8_t *parse_hex_str(const char *value, size_t str_len)
-        {
-            size_t len = str_len / 2;
-            uint8_t *buff = (uint8_t *)malloc(sizeof(uint8_t) * len);
-            for (size_t i = 0; i < len; i++)
-                buff[i] = (parse_hex(value[i * 2]).value() << 4) | parse_hex(value[i * 2 + 1]).value();
+        uint8_t *parse_hex_str(const char *value, size_t str_len);
+        int parse_int(uint8_t *data, int start_pos);
+        bool parse_bit(uint8_t value, int pos);
 
-            return buff;
-        }
-
-        static int parse_int(uint8_t *data, int start_pos)
-        {
-            return int(data[start_pos] << 24 | data[start_pos + 1] << 16 | data[start_pos + 2] << 8 | data[start_pos + 3]);
-        }
-
-        static bool parse_bit(uint8_t value, int pos)
-        {
-            return (value & (1 << pos)) >> pos;
-        }
-
-        static void reverse_chunks(uint8_t *data, int len, uint8_t *reversed_buf)
-        {
-            for (int i = 0; i < len; i += 4)
-            {
-                int l = MIN(4, len - i); // limit for a chunk, 4 or what's left
-                for (int j = 0; j < l; j++)
-                {
-                    reversed_buf[i + j] = data[i + (l - 1 - j)];
-                }
-            }
-        }
+        void reverse_chunks(uint8_t *data, int len, uint8_t *reversed_buf);
     }
 }
