759b319f879bb0e52a8b671fd2fd56e7917ed727
gerge <120474+gerge@users.noreply.github.com>
2022-10-10 17:50:55 +0200

    esphome 2022.9.3

--- components/danfoss_eco/properties.cpp
+++ components/danfoss_eco/properties.cpp
@@ -26,8 +26,8 @@ namespace esphome
 
         bool DeviceProperty::read_request(BLEClient *client)
         {
-            auto status = esp_ble_gattc_read_char(client->gattc_if,
-                                                  client->conn_id,
+            auto status = esp_ble_gattc_read_char(client->get_gattc_if(),
+                                                  client->get_conn_id(),
                                                   this->handle,
                                                   ESP_GATT_AUTH_REQ_NONE);
             if (status != ESP_OK)
@@ -40,8 +40,8 @@ namespace esphome
         {
             ESP_LOGD(TAG, "[%s] write_request: handle=%#04x, data=%s", this->component_->get_name().c_str(), this->handle, hexencode(data, data_len).c_str());
 
-            auto status = esp_ble_gattc_write_char(client->gattc_if,
-                                                   client->conn_id,
+            auto status = esp_ble_gattc_write_char(client->get_gattc_if(),
+                                                   client->get_conn_id(),
                                                    this->handle,
                                                    data_len,
                                                    data,
